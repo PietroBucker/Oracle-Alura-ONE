9c874051e063a8752a60a028bc0f2f9fd4acb04c
Pietro Bucker <pietro_177@hotmail.com>
2024-05-14 22:50:13 -0300

    FEAT: criado a funçao para cadastro

--- Challenge-Alura-Geek/js/connection.js
+++ Challenge-Alura-Geek/js/connection.js
@@ -1,12 +1,25 @@
 async function requisicaoGet() {
 	const response = await fetch('http://localhost:3000/produtos');
 	const data = await response.json();
-	console.log(data);
 	return data;
 }
 
-requisiGet();
+async function requisicaoPost(produto) {
+	await fetch('http://localhost:3000/produtos', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(produto),
+	});
+	return 'ok';
+}
+
+// async function requisicaoDelete(id) {
+
+// }
 
 export {
 	requisicaoGet,
+	requisicaoPost,
 };
