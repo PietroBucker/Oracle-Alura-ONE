900eee3634dba5dd797450bdfae27cd22b4cc017
Pietro Bucker <pietro_177@hotmail.com>
2024-05-20 23:37:33 -0300

    REFACT: criado arquivo separado para Get

---
+++ Challenge-Alura-Geek/js/GET.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line import/extensions
+import { requisicaoGet } from './connection.js';
+
+const semFoto = '/assets/semFoto.png';
+
+function isValidImageUrl(url) {
+	// Expressão regular para validar o formato do URL da imagem
+	const imageUrlRegex = /\.(jpeg|jpg|gif|png)$/i;
+	return imageUrlRegex.test(url);
+}
+
+const renderCards = async () => {
+	const cardsContent = document.querySelector('.cards_content');
+	const { data } = await requisicaoGet();
+	console.log(data);
+	cardsContent.innerHTML = '';
+	data.forEach((item) => {
+		if (!isValidImageUrl(item.imagem)) {
+			item.imagem = semFoto;
+		}
+
+		cardsContent.innerHTML += `
+			<div class="card">
+				<img 
+					src="${item.imagem}" 
+					alt="Imagem do produto" 
+				>
+				<p>${item.nome}</p>
+				<p><strong>R$ ${item.preco}</strong></p>
+				<div class="btns_card">
+					<button class="btn_favoritar">🌟</button>
+					<button class="btn_remover">✖️</button>
+				</div>
+			</div>
+        `;
+	});
+};
+
+export default renderCards;
