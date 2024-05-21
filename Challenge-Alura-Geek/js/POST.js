3eb3f64b16b1eb26f3b04d94bdc22e8c53cf8f8f
Pietro Bucker <pietro_177@hotmail.com>
2024-05-20 23:37:59 -0300

    REFACT: criado arquivo separado para Post

---
+++ Challenge-Alura-Geek/js/POST.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/extensions */
+import { requisicaoPost } from './connection.js';
+
+const btnAdicionar = document.querySelector('.btn_adicionar');
+const inputs = document.querySelectorAll('input');
+const form = document.querySelector('#form_product');
+
+function validaForm() {
+	const charValidate = 3;
+	inputs.forEach((input) => {
+		input.addEventListener('input', () => {
+			const next = input.nextElementSibling;
+			if (input.value.length < charValidate) {
+				next.style.display = 'block';
+			} else {
+				next.style.display = 'none';
+			}
+		});
+	});
+
+	form.addEventListener('input', () => {
+		btnAdicionar.disabled = Array.from(inputs)
+			.some((input) => input.value.length < charValidate);
+	});
+}
+
+btnAdicionar.addEventListener('click', async (event) => {
+	event.preventDefault();
+	console.log('teste');
+	const nome = document.querySelector('input[name="nome"]').value;
+	const preco = Number(document.querySelector('input[name="preco"]').value).toFixed(2);
+	const imagem = document.querySelector('input[name="imagem"]').value;
+
+	await requisicaoPost({ nome, preco, imagem });
+	await renderCards();
+});
+
+export default validaForm;
