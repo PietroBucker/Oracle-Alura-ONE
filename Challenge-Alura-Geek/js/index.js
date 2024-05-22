e41e2507832da945cf9016eaf87aa58c4c9b51fd
Pietro Bucker <pietro_177@hotmail.com>
2024-05-21 22:43:56 -0300

    ADD: add novas chamadas de funcao no index

--- Challenge-Alura-Geek/js/index.js
+++ Challenge-Alura-Geek/js/index.js
@@ -1,8 +1,12 @@
 /* eslint-disable import/extensions */
 import renderCards from './GET.js';
 import validaForm from './POST.js';
+import removeCard from './DEL.js';
+import favoritar from './favoritar.js';
 
 window.onload = async () => {
 	await renderCards();
+	await removeCard();
 	validaForm();
+	favoritar();
 };
