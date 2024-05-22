d8af01f24381918aaa1617a55dd229cbb9d7d62f
Pietro Bucker <pietro_177@hotmail.com>
2024-05-21 22:42:50 -0300

    FEAT: criado funcao para deletar

---
+++ Challenge-Alura-Geek/js/DEL.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/extensions */
+import { requisicaoDelete } from './connection.js';
+
+async function removeCard() {
+	const btnRemover = document.querySelectorAll('.btn_remover');
+	btnRemover.forEach((btn) => {
+		btn.onclick = () => requisicaoDelete(btn.id);
+	});
+}
+export default removeCard;
