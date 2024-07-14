add1adcf310a2dc9bb97c26b8fad1f89eb2016a7
Pietro Bucker <pietro_177@hotmail.com>
2024-07-14 17:07:38 -0300

    Organizacao novapagina

--- AluraFlix/src/pages/NovoVideo/index.jsx
+++ AluraFlix/src/pages/NovoVideo/index.jsx
@@ -1,13 +1,20 @@
 import Style from './NovoVideo.module.css';
 import Form from '../../components/Form';
+import Button from '../../components/Button';
 
 export default function NovoVideo() {
   return (
     <div className={ Style.container }>
-      <h1>Novo vídeo</h1>
-      <p>Preencha os campos abaixo para adicionar um novo vídeo.</p>
+      <div className={ Style.containerText }>
+        <h1>Novo vídeo</h1>
+        <p>Preencha os campos abaixo para adicionar um novo vídeo.</p>
+      </div>
       <h2>Criar Card</h2>
       <Form type="novo-video" />
+      <div className={ Style.buttonContainer }>
+        <Button type="blue" textColor="white">Guardar</Button>
+        <Button>Limpar</Button>
+      </div>
     </div>
   );
 }
