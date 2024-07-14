3a3f2786d091ffe8aba0bd29503352ae5e129f4e
Pietro Bucker <pietro_177@hotmail.com>
2024-07-14 17:05:09 -0300

    ajuste compenentt button

--- AluraFlix/src/components/Button/index.jsx
+++ AluraFlix/src/components/Button/index.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 import Style from './Button.module.css';
 
-export default function Button({ type, children, ...rest }) {
+export default function Button({ type = 'white', textColor, children, ...rest }) {
   return (
     <Link { ...rest }>
       <button
         className={ `${Style[type]} ${Style.button}` }
+        style={ { color: `${textColor}` } }
       >
         {children}
       </button>
