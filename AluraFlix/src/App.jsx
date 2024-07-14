37601b1678b7ab7644ca89ddfb327a38ebc2bf49
Pietro Bucker <pietro_177@hotmail.com>
2024-07-14 18:39:38 -0300

    add provider ao app

--- AluraFlix/src/App.jsx
+++ AluraFlix/src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import Layout from '@/pages/Layout';
 import NovoVideo from './pages/NovoVideo';
+import { VideoProvider } from './context/VideoContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <Layout /> }>
-          <Route index element={ <Home /> } />
-          <Route path="novo-video" element={ <NovoVideo /> } />
-        </Route>
-      </Routes>
+      <VideoProvider>
+
+        <Routes>
+          <Route path="/" element={ <Layout /> }>
+            <Route index element={ <Home /> } />
+            <Route path="novo-video" element={ <NovoVideo /> } />
+          </Route>
+        </Routes>
+      </VideoProvider>
 
     </BrowserRouter>
   );
